6222eda638b65e9942e50ecca96c5ae641f23eb6
Chris Rau <98765332+rau03@users.noreply.github.com>
2025-01-29 20:39:12 -0600

    first commit setup

---
+++ src/sections/Header.tsx
@@ -0,0 +1,60 @@
+import { header } from "motion/react-client";
+import { FC } from "react";
+
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+const navItems = [
+  {
+    label: "About",
+    href: "#intro",
+  },
+  {
+    label: "Selected Works",
+    href: "#projects",
+  },
+  {
+    label: "Testimonials",
+    href: "#testimonials",
+  },
+  {
+    label: "FAQs",
+    href: "#faqs",
+  },
+  {
+    label: "Contact",
+    href: "#contact",
+  },
+];
+
+const Header: FC = () => {
+  return (
+    <header>
+      <div className="container !max-w-full">
+        <div className="flex justify-between h-20 items-center">
+          <div>
+            <a href="/">
+              <span className="text-xl font-bold uppercase">
+                Christopher Rau
+              </span>
+            </a>
+          </div>
+          <div>
+            <div className="size-11 border border-stone400 rounded-full inline-flex items-center justify-center">
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect x="3" y="15" width="18" height="2" fill="currentColor" />
+                <rect x="3" y="7" width="18" height="2" fill="currentColor" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
